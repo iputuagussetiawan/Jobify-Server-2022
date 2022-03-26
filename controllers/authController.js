61920aa77931fd79b6e54488f36a6c1b70aa654a
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-26 16:45:58 +0800

    Validate Email and Basic Register Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
-
-const register =(req, res)=>{
-  res.send('register user')
+import User from '../models/User.js'
+const register =async(req, res)=>{
+  try {
+    const user = await User.create(req.body)
+    res.status(201).json({user})
+  } catch (error) {
+    res.status(500).json({msg:'There wa an error'})
+  }
 }
 
 const login =(req, res)=>{
