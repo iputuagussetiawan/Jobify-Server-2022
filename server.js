b40c5f1468583ee2fabb49f9c05345e526e4bb98
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-26 13:19:07 +0800

    Connect To DB

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
+import connectDB from './db/connect.js';
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import dotenv from 'dotenv'
+
 dotenv.config()
 const app = express();
 
 app.get("/", (req, res) => {
-    throw new Error('error')
+    //throw new Error('error')
 	res.send("Welcome!");
 });
 
@@ -15,4 +17,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URL)
+      app.listen(port, () => {
+        console.log(`Server is listening on port ${port}...`)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+}
+  
+start()
