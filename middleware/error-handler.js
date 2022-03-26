981f3213300277246df54aeb5332bc92f1361175
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-26 18:23:19 +0800

    error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,7 +6,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     StatusCode:StatusCodes.INTERNAL_SERVER_ERROR,
     msg:'Someting went wrong, try again later'
  }
- res.status(defaultError.StatusCode).json({msg:err});
+ if(err.name==='ValidationError'){
+    defaultError.StatusCode=StatusCodes.BAD_REQUEST
+    defaultError.msg=err.message
+ }
+ res.status(defaultError.StatusCode).json({msg:defaultError.msg});
 }
 
 export default errorHandlerMiddleware
