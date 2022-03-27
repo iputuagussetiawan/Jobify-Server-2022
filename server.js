760731972d81785cf0563a0c8717cdefb650bfc1
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-27 14:52:18 +0800

    Morgan NPM Package

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const app = express();
 import dotenv from 'dotenv'
 dotenv.config()
 import 'express-async-errors';
+import morgan from 'morgan';
 import connectDB from './db/connect.js';
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import authRouter from "./routes/authRoutes.js";
 import jobsRouter from "./routes/jobsRoutes.js";
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json());
 
