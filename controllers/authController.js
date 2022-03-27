6d2d3c5d1051eecfc8086fd5110bce7206590995
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-27 15:11:33 +0800

    Login Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,8 +26,24 @@ const register =async(req, res)=>{
   })
 }
 
-const login =(req, res)=>{
-  res.send('login user')
+const login =async(req, res)=>{
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({ email }).select('+password')
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+  //res.send('login user')
+  const token = user.createJWT()
+  user.password = undefined
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
 const updateUser =(req, res)=>{
