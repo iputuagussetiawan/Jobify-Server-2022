760fdc8782509b770f1f1548a910eb7fd1620ed8
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-17 16:21:50 +0800

    Jobs Container

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const Job = () => {
-  return (
-    <div>Single Job</div>
-  )
-}
+const Job = ({ company }) => {
+	return <h5>{company}</h5>;
+};
 
 export default Job
