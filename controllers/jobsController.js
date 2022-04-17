afaeabf523987631fa0bc37869f3d26859468b1c
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-17 12:35:04 +0800

    CreateJob Model & Controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,26 +1,33 @@
+import Job from '../models/Job.js'
+import { StatusCodes } from 'http-status-codes'
+import {
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError,
+} from '../errors/index.js'
 
-const createJob=async (req, res)=>{
-  res.send('create job');
+
+const createJob = async (req, res) => {
+  const { position, company } = req.body
+
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values')
+  }
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
-
-
-const getAllJobs=async (req, res)=>{
-  res.send('Get All  job');
+const getAllJobs = async (req, res) => {
+ 
 }
-
-
-const updateJob=async (req, res)=>{
-  res.send('update job');
+const updateJob = async (req, res) => {
+  
 }
-
-
-const deleteJob=async (req, res)=>{
-  res.send('delete job');
+const deleteJob = async (req, res) => {
+ 
 }
-
-
-const showStats=async (req, res)=>{
-  res.send('show status job');
+const showStats = async (req, res) => {
+  
 }
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats }
